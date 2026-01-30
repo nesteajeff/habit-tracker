3e370247b89881e0f76cb5a683b635fdf4e4aa61
Jeffrey Peng <jeffreypeng2@gmail.com>
2026-01-30 17:40:16 -0500

    add cors so that browser post is not blocked

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
 import habitsRouter from "./routes/habits";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use("/habits", habitsRouter);
 
