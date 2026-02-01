694515daf4576c894fa2fd2570e83f1be35f9901
Jeffrey Peng <jeffreypeng2@gmail.com>
2026-01-31 23:42:24 -0500

    add amplify dependency

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
+import { withAmplify } from "@aws-amplify/adapter-nextjs";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig;
+export default withAmplify(nextConfig);
