dbb90ffa0d79458009d5649e1e4db69ac92e9eeb
Jeffrey Peng <jeffreypeng2@gmail.com>
2026-02-03 00:09:21 -0500

    only use ssl if non-local

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,8 +1,13 @@
 import { Pool } from "pg";
 
+const connectionString = process.env.DATABASE_URL;
+const isLocalConnection =
+  connectionString?.includes("localhost") ||
+  connectionString?.includes("127.0.0.1");
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  connectionString,
+  ssl: isLocalConnection ? undefined : { rejectUnauthorized: false },
 });
 
 export default pool;
